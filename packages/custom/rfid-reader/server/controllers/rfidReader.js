0882cd1b19925223c8d80c27106436c7288230e0
Oleksii <oleksii.s@myplanetdigital.com>
2015-05-05 15:45:43 +0300

    GC: controller changed

--- packages/custom/rfid-reader/server/controllers/rfidReader.js
+++ packages/custom/rfid-reader/server/controllers/rfidReader.js
@@ -4,58 +4,56 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-//    Person = mongoose.model('Person');
-//    Resource = mongoose.model('Resource'),
+    Person = mongoose.model('Person'),
+    Resource = mongoose.model('Resource'),
     Tag = mongoose.model('Tag');
-//    _ = require('lodash');
-//var endpoints = {
-//  library: {
-//    description: "Local library",
-//    url: "library",
-//    message: "Welcome to our library"
-//  },
-//  smth: {
-//    description: "Some other place",
-//    url: "smth",
-//    message: "Welcome to the SMTH"
-//  }
-//};
+
 /**
  * Find RFID
  */
 exports.entry = function(req, res) {
-//  var pers = new Person({
-//    name: 'Vasya',
-//    children: [{
-//      _id: 123,
-//      type: 'person'
-//    }]
-//  });
-//
-//  pers.save(function(err) {
-//    if (err) {
-//      console.log('fucking err: ' + err);
-//    }
-//    console.log(err);
-//    console.log('cool');
-//  });
-
   var rfid = req.param('rfid');
   console.log('RFID: ' + rfid);
   Tag.find({_id: rfid}).limit(1).exec(function(err, result) {
-    console.log('err: ' + err);
     if (err) {
       return res.status(500).json({
-        error: 'Cannot list the articles'
+        error: 'Cannot get RFID'
       });
     }
     // If query result contain data (array not empty) RFID is exists in database
     if (result.length > 0) {
-      res.json(result[0]);
+      var tag = result[0];
+      switch (tag.type) {
+        case 'person':
+          Person.find({tag: tag._id}).limit(1).exec(function(err, result) {
+            if (err) {
+              return res.status(500).json({
+                error: 'Cannot get person'
+              });
+            }
+
+          });
+          break;
+        case 'resource':
+          Resource.find({tag: tag._id}).limit(1).exec(function(err, result) {
+            if (err) {
+              return res.status(500).json({
+                error: 'Cannot get resource'
+              });
+            }
+          });
+          break;
+        default:
+          return res.status(500).json({
+            error: 'Wrong RFID type'
+          });
+      }
     }
     // RFID is not exists in database so will create it
     else {
-      res.json(404, { message: 'ID not found.' });
+      res.status(404).json({
+        message: 'ID not found.'
+      });
     }
   });
 };
